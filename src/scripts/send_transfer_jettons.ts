0147f369def0147bea39d68e2258ba86e18baaf4
iO <senpai9464@yandex.ru>
2025-05-13 23:02:34 +0300

    fix: update minimum storage and commission amounts, enhance balance checks in contract logic

--- src/scripts/send_transfer_jettons.ts
+++ src/scripts/send_transfer_jettons.ts
@@ -10,8 +10,11 @@ export async function run(provider: NetworkProvider) {
   );
 
   const dictionary = createJettonsDictionary([
-    { address: Address.parse('0QAYRw04JzUo1IEK6TL6vKfos66gsdN6vUFfJeA3OOjOfDPG'), amount: toNano('0.2') },
-    { address: Address.parse('0QD-q5a1Z3kYfDBgYUcUX_MigynA5FuiNx0i5ySt37rfrFeP'), amount: toNano('0.3') }, //v5
+    { address: Address.parse('0QAYRw04JzUo1IEK6TL6vKfos66gsdN6vUFfJeA3OOjOfDPG'), amount: toNano('0.1') },
+    { address: Address.parse('0QD-q5a1Z3kYfDBgYUcUX_MigynA5FuiNx0i5ySt37rfrFeP'), amount: toNano('0.2') },
+    { address: Address.parse('0QC9vm__DOB74-HkN9pxfMDMLYT4YlDPYj54dZ9yqvsgXYpZ'), amount: toNano('0.3') },
+    { address: Address.parse('0QC3PUCoxBdLfOmO8xFQ84TGFPQUatxvvRsSAODKEvjbb4OS'), amount: toNano('0.4') },
+    { address: Address.parse('0QCmnBX_oEJ9kkeKV6uG9ifl-uEdC_SKGN_Q1EwYKh171brP'), amount: toNano('0.5') },
   ]);
 
   await contractGamlerDefi.sendTransferJettons(provider.sender(), dictionary);
